ebfb75f6db2b82492daaff4f85f368e76e9835af
jisu <ahteostnqls@naver.com>
2024-01-29 23:21:17 +0900

    feat: 상단 이미지 영역 컴포넌트 작업

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import Header from 'components/Header/Header'
+import TopBanner from 'components/TopBanner/TopBanner'
 import React from 'react'
 
 export default function Layout({children}) {
   return (
     <>
       <Header />
+      <TopBanner />
       <main>
         {children}
       </main>
