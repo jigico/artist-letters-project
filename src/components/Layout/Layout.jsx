b7eb8d92dbc861e9e267a5ae1c77c5fb37eaca7f
jisu <ahteostnqls@naver.com>
2024-01-30 14:17:53 +0900

    design: layout 스타일 추가

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,15 +2,16 @@ import Footer from 'components/Footer/Footer'
 import Header from 'components/Header/Header'
 import TopBanner from 'components/TopBanner/TopBanner'
 import React from 'react'
+import { Layout1280 } from './LayoutStyles'
 
 export default function Layout({children}) {
   return (
     <>
       <Header />
       <TopBanner />
-      <main>
+      <Layout1280>
         {children}
-      </main>
+      </Layout1280>
       <Footer />
     </>
   )
