b642f69fb4f177d93b4ff18aa14971dc4d801d3d
jisu <ahteostnqls@naver.com>
2024-02-01 03:42:01 +0900

    feat: 상세 화면에 보일 데이터 조회

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import data from '../shared/fakeData.json'
+import LetterContent from "components/LetterDetail/LetterContent";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Detail() {
-  const params = useParams();
-  const findData = data.find((item) => item.id === params.id)
+  const LOCAL_KEY = "letter"; //localStorage key
+  const initial = JSON.parse(localStorage.getItem(LOCAL_KEY));
+  const [data, setData] = useState(initial !== null ? initial : []); //TODO : 이전 데이터 체크를 useEffect 를 사용해야 하는건지 확인 필요
 
-  return (
-    <>
-      {findData.content}
-    </>
-  )
+  const params = useParams();
+  let findData = {};
+
+  //id랑 일치하는 데이터 찾기
+  for (const value of Object.values(data)) {
+    for (let i = 0; i < value.length; i++) {
+      console.log(value[i]);
+      if (value[i].id === params.id) {
+        findData = value[i];
+        break;
+      }
+    }
+  }
+
+  return <LetterContent data={findData} setData={setData} />;
 }
