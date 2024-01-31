eee4934e78c398bc9342f33a02f00b3fab480c17
jisu <ahteostnqls@naver.com>
2024-02-01 00:10:00 +0900

    feat: 버튼 컴포넌트 props 추가에 따른 입력 폼 컴포넌트의 버튼 display props 추가

--- src/components/LetterForm/LetterForm.jsx
+++ src/components/LetterForm/LetterForm.jsx
@@ -1,14 +1,14 @@
-import React, { useRef, useState } from 'react';
-import { FormContainer, LetterInput, LetterLabel, LetterTextarea } from './LetterFormStyles';
-import Button from 'components/Button/Button';
-import LetterSelect from './LetterSelect';
-import { memberData } from 'shared/memberData';
-import {v4 as uuidv4} from 'uuid';
+import React, { useRef, useState } from "react";
+import { FormContainer, LetterInput, LetterLabel, LetterTextarea } from "./LetterFormStyles";
+import LetterSelect from "./LetterSelect";
+import { memberData } from "shared/memberData";
+import { v4 as uuidv4 } from "uuid";
+import { ButtonBox, StyledButton } from "components/Button/ButtonStyles";
 
-export default function LetterForm({data, setData, localKey}) {
-  const [selected, setSelected] = useState('');
-  const nameRef = useRef('');
-  const contentsRef = useRef('');
+export default function LetterForm({ data, setData, localKey }) {
+  const [selected, setSelected] = useState("");
+  const nameRef = useRef("");
+  const contentsRef = useRef("");
 
   //폼 이벤트
   const formHandler = (e) => {
@@ -17,45 +17,44 @@ export default function LetterForm({data, setData, localKey}) {
     const contents = e.target.contents.value;
 
     //유효성 검사
-    if(!selected.trim()){
-      alert('아티스트를 선택해주세요');
+    if (!selected.trim()) {
+      alert("아티스트를 선택해주세요");
       //TODO: useRef 로 포커스 이동
       return;
     }
-    if(!name.trim()){
-      alert('닉네임을 입력해주세요');
+    if (!name.trim()) {
+      alert("닉네임을 입력해주세요");
       nameRef.current.focus();
       return;
     }
-    if(!contents.trim()){
-      alert('컨텐츠를 입력해주세요');
+    if (!contents.trim()) {
+      alert("컨텐츠를 입력해주세요");
       contentsRef.current.focus();
       return;
     }
 
     //등록 기능
     saveLetter(name, contents);
-    
-    alert('등록이 완료되었습니다.');
+
+    alert("등록이 완료되었습니다.");
     e.target.reset();
   };
 
   //아티스트 셀렉트박스 이벤트
   const onChangeHandler = (e) => {
     setSelected(e.target.value);
-  }
-
+  };
 
   //등록 기능
   const saveLetter = (name, contents) => {
     const id = uuidv4();
     const date = new Date();
-    const newDataArr = data; 
-    
+    const newDataArr = data;
+
     const newDataObj = {
       createdAt: date.toLocaleString(),
       nickname: name,
-      avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/36.jpg',
+      avatar: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/36.jpg",
       content: contents,
       writedTo: selected,
       id
@@ -67,7 +66,6 @@ export default function LetterForm({data, setData, localKey}) {
     setData([...newDataArr]);
   };
 
-
   return (
     <FormContainer onSubmit={formHandler}>
       <LetterSelect id="artistSelect" onChangeHandler={(e) => onChangeHandler(e)} memberData={memberData}></LetterSelect>
@@ -75,7 +73,9 @@ export default function LetterForm({data, setData, localKey}) {
       <LetterInput type="text" id="name" name="name" ref={nameRef} maxLength="10" />
       <LetterLabel htmlFor="contents">내용</LetterLabel>
       <LetterTextarea name="contents" id="contents" cols="30" rows="10" maxLength="70" ref={contentsRef}></LetterTextarea>
-      <Button>등록</Button>
+      <ButtonBox>
+        <StyledButton display={true}>등록</StyledButton>
+      </ButtonBox>
     </FormContainer>
-  )
+  );
 }
