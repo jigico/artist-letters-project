226530db41178c74aa03c15f6bee00f290670a44
jisu <ahteostnqls@naver.com>
2024-02-01 01:43:45 +0900

    style: 코드 포맷 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import LetterList from 'components/Letter/LetterList'
-import LetterForm from 'components/LetterForm/LetterForm'
-import MemberList from 'components/Members/MemberList'
-import React, { useState } from 'react'
-
+import LetterList from "components/Letter/LetterList";
+import LetterForm from "components/LetterForm/LetterForm";
+import MemberList from "components/Members/MemberList";
+import React, { useState } from "react";
 
 export default function Home() {
-  const LOCAL_KEY = 'letter'; //localStorage key
+  const LOCAL_KEY = "letter"; //localStorage key
   const initial = JSON.parse(localStorage.getItem(LOCAL_KEY));
   const [data, setData] = useState(initial !== null ? initial : []); //TODO : 이전 데이터 체크를 useEffect 를 사용해야 하는건지 확인 필요
 
@@ -15,5 +14,5 @@ export default function Home() {
       <MemberList />
       <LetterList data={data} />
     </>
-  )
+  );
 }
