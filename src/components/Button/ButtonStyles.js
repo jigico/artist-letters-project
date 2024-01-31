81c112f66adc6ef23bcb83a5b0ae962e146ed937
jisu <ahteostnqls@naver.com>
2024-02-01 00:09:08 +0900

    feat: 버튼 컴포넌트 props 추가

--- src/components/Button/ButtonStyles.js
+++ src/components/Button/ButtonStyles.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 
+export const ButtonBox = styled.div`
+  margin-top: 10px;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  gap: 10px;
+`;
 export const StyledButton = styled.button`
+  display: ${(props) => (props.display ? "block" : "none")};
   height: 25px;
   color: #fff;
   background-color: #0066ff;
