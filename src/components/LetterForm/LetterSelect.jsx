586580034553e08d87963cc2daba95dc59ae146d
jisu <ahteostnqls@naver.com>
2024-02-01 01:16:39 +0900

    feat: 아티스트 선택 value 변경 artist -> id

--- src/components/LetterForm/LetterSelect.jsx
+++ src/components/LetterForm/LetterSelect.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { LetterLabel, SelectStyle } from './LetterFormStyles';
+import React from "react";
+import { LetterLabel, SelectStyle } from "./LetterFormStyles";
 
-export default function LetterSelect({memberData, onChangeHandler}) {
+export default function LetterSelect({ memberData, onChangeHandler }) {
   return (
     <>
-      <LetterLabel htmlFor='artistSelect'>아티스트</LetterLabel>
-      <SelectStyle id='artistSelect' onChange={onChangeHandler}>
+      <LetterLabel htmlFor="artistSelect">아티스트</LetterLabel>
+      <SelectStyle id="artistSelect" onChange={onChangeHandler}>
         <option value="">아티스트를 선택해주세요.</option>
-        {
-          memberData.map((item) => {
-            return <option key={item.id} value={item.artist}>{item.artist}</option>
-          })
-        }
+        {memberData.map((item) => {
+          return (
+            <option key={item.id} value={item.key}>
+              {item.artist}
+            </option>
+          );
+        })}
       </SelectStyle>
     </>
-  )
+  );
 }
